e7e6a616a189e239c3c6e9b8939db1b53b68bd25
Scott Bowen <scottbowen@EKN-OSX100.foothills.net>
2021-06-13 16:39:10 -0400

    init project & added app icons

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Scott Bowen on 6/13/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

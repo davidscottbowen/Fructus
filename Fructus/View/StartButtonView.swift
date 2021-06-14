cd61937aba83d76ae1747384bc2435c53d51afab
Scott Bowen <scottbowen@EKN-OSX100.local>
2021-06-14 11:21:58 -0400

    started blueberry screen

---
+++ Fructus/View/StartButtonView.swift
@@ -0,0 +1,25 @@
+//
+//  StartButtonView.swift
+//  Fructus
+//
+//  Created by Scott Bowen on 6/14/21.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+struct StartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButtonView()
+    }
+}
